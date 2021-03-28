1dc92ebdccbebb30fb652af8de6200662c924104
lijiaocn <lijiaocn@foxmail.com>
2021-03-28 18:00:25 +0800

    add

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@
 
 port=`cat port`
 rm -rf /tmp/jekyll.log
-nohup bundle exec jekyll serve --port $port --incremental  2>&1 1 > /tmp/jekyll.log &
+nohup bundle exec jekyll serve --port $port --incremental  --force_polling 2>&1 1 > /tmp/jekyll.log &
 #nohup bundle exec jekyll serve --port $port  2>&1 1 > /tmp/jekyll.log &
 #bundle exec jekyll serve --port 4004 
