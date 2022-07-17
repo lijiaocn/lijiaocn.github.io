bd9875ea2bc1277c69d88e4efac3ac6756eada81
lijiaocn <lijiaocn@foxmail.com>
2022-07-17 18:47:36 +0800

    m

---
+++ js/body20220717.js
@@ -0,0 +1,24 @@
+/*点击后，收起nav下拉列表*/
+$('#navbarcolumn a').on('click', function(){
+	$('.btn-navbar').click(); //bootstrap 2.x
+	$('.navbar-toggle').click() //bootstrap 3.x by Richard
+});
+
+
+function DetectAds(){
+    innerHtml=""
+    ads=document.querySelectorAll('.adsbygoogle')
+    ads.forEach((ad,index,ads)=>{
+        innerHtml=innerHtml+ad.innerHTML
+    })
+    if(innerHtml ==''){
+        r=confirm('访客您好，您需要在广告屏蔽插件中排除此网站，排除后点击确定')
+        if (r==true){
+            location.reload()
+        }else{
+            setTimeout(DetectAds, 5000)
+        }
+    }
+}
+
+setTimeout(DetectAds,5000)
