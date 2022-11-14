b5f8efa5bdcf0c357eb87dba62cee296a0f57e03
lijiaocn <lijiaocn@foxmail.com>
2022-11-14 15:07:44 +0800

    m

---
+++ js/body20221114.js
@@ -0,0 +1,22 @@
+/*点击后，收起nav下拉列表*/
+$('#navbarcolumn a').on('click', function(){
+	$('.btn-navbar').click(); //bootstrap 2.x
+	$('.navbar-toggle').click() //bootstrap 3.x by Richard
+});
+
+function DetectAds(){
+    innerHtml=""
+    ads=document.querySelectorAll('.adsbygoogle')
+    ads.forEach((ad,index,ads)=>{
+        innerHtml=innerHtml+ad.innerHTML
+    })
+    if(innerHtml ==''){
+        r=confirm('解除广告插件对对本站屏蔽!')
+        if (r==true){
+            setTimeout(DetectAds, 30000)
+        }else{
+            setTimeout(DetectAds, 30000)
+        }
+    }
+}
+setTimeout(DetectAds,20000)
