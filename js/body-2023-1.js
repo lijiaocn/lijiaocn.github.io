33b8f4b0bd78f295c3a6ef2963f77d6ec4b7e62c
lijiaocn <lijiaocn@foxmail.com>
2023-07-28 11:50:35 +0800

    m

--- js/body-2023-1.js
+++ js/body-2023-1.js
@@ -22,3 +22,6 @@ function DetectAds(){
 }
 setTimeout(DetectAds,20000)
 */
+
+/*Google 广告拦截收入挽回代码*/
+<script async src="https://fundingchoicesmessages.google.com/i/pub-8176866190626448?ers=1" nonce="9DYNtNbey7d75wS-aLUAUQ"></script><script nonce="9DYNtNbey7d75wS-aLUAUQ">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>
